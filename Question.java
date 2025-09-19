57e20c771b9c02c6577f026754c5cd8c63f84d68
Rakesh Singh <rakeshsingh0156@gmail.com>
2025-09-19 08:33:24 +0530

    Please enter the commit message for your changes. Lines starting

---
+++ Question.java
@@ -0,0 +1,28 @@
+public class Question{
+    private int id;
+    private String question;
+    private String opt1;
+    private String opt2;
+    private String opt3;
+    private String opt4;
+    private String answer;
+    //private elements can be accessable with getter and setter (encapsulation)
+    public int getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getQuestin(){
+        return this.question;
+    }
+    public void setQuestion(String question){
+        this.question=question;
+    }
+    public String getOpt1(){
+        return this.opt1;
+    }
+    
+
+
+}
